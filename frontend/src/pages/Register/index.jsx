a4c04a4587473ce01d5050bd78b9902e43d93015
DouglasSerena <douglas222serena@rede.ulbra.br>
2020-07-26 22:56:21 -0300

    frontend finish

---
+++ frontend/src/pages/Register/index.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { FiArrowLeft } from "react-icons/fi";
+import { Link, useHistory } from "react-router-dom";
+
+import "./style.css";
+import logoImg from "./../../assets/logo.svg";
+
+import api from "./../../services/api";
+
+export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [whatsapp, setWhatsapp] = useState("");
+  const [city, setCity] = useState("");
+  const [uf, setUf] = useState("");
+
+  const history = useHistory();
+
+  async function handleRegister(event) {
+    event.preventDefault();
+    const datas = {
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
+    };
+
+    try {
+      const response = await api.post("ongs", datas);
+
+      alert(`seu id é ${response.data.id}`);
+
+      history.push("/");
+    } catch (error) {
+      alert(`error tente novamente`);
+    }
+  }
+
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero" />
+
+          <h1>Cadastro</h1>
+          <p>
+            Faça seu cadastro, entre na plataform e ajude pessoas a encontrarem
+            os casos da sua ONG.
+          </p>
+
+          <Link to="/" className="back-link">
+            <FiArrowLeft size={16} color="#E02041" />
+            Voutar para o logon
+          </Link>
+        </section>
+        <form onSubmit={handleRegister}>
+          <input
+            placeholder="Nome da ONG"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="WhatsApp"
+            value={whatsapp}
+            onChange={(e) => setWhatsapp(e.target.value)}
+          />
+
+          <div className="input-group">
+            <input
+              placeholder="Cidade"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <input
+              placeholder="UF"
+              style={{ width: 80 }}
+              value={uf}
+              onChange={(e) => setUf(e.target.value)}
+            />
+          </div>
+
+          <button className="button" type="submit">
+            Cadastar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ readme.md
@@ -0,0 +1,88 @@
+<p align="center">
+  <img alt="BeTheHero" src="frontend/src/assets/logo.svg" width="100%">
+</p>
+
+<h4 align="center">
+  🚀 Semana OmniStack 11.0
+</h4>
+
+<p align="center">
+ <img src="https://img.shields.io/static/v1?label=PRs&message=welcome&color=7159c1&labelColor=000000" alt="PRs welcome!" />
+
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=7159c1&labelColor=000000">
+</p>
+
+<p align="center">
+  <a href="#rocket-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-como-contribuir">Como contribuir</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<br>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [Node.js](https://nodejs.org/en/)
+- [React](https://reactjs.org)
+- [React Native](https://facebook.github.io/react-native/)
+- [Expo](https://expo.io/)
+
+## 💻 Projeto
+
+O BeTheHero é um projeto que visa conectar pessoas que desejam fazer contribuições monetárias a ONG's (Organizações não governamentais) que precisam de ajuda.
+
+## 🔖 Layout
+
+Você pode visualizar o layout do projeto no formato através [desse link](https://www.figma.com/file/2C2yvw7jsCOGmaNUDftX9n/Be-The-Hero---OmniStack-11?node-id=37%3A394). Lembrando que você irá precisar ter uma conta no [Figma](http://figma.com/).
+
+## 🤔 Como contribuir
+
+- Faça um fork desse repositório;
+- Cria uma branch com a sua feature: `git checkout -b minha-feature`;
+- Faça commit das suas alterações: `git commit -m 'feat: Minha nova feature'`;
+- Faça push para a sua branch: `git push origin minha-feature`.
+
+Depois que o merge da sua pull request for feito, você pode deletar a sua branch.
+
+## 🔖 Como Executar
+
+#### Clonando o projeto
+
+```sh
+git clone https://github.com/DouglasSerena/Omnistack11.git
+cd Omnistack11
+npm install / yarn install
+```
+
+#### Iniciando Backend
+
+```sh
+cd backend
+npm start / yarn start
+```
+
+#### Iniciando Frontend
+
+```sh
+cd frontend
+npm start / yarn start
+```
+
+#### Iniciando mobile
+
+```sh
+cd mobile
+npm start / yarn start
+```
+
+<p align="center">
+  <img alt="Frontend" src="frontend/src/assets/heroes.png" width="596px" height="574px">
+</p>
+
+---
+
+Feito com ♥ by Douglas Serena 🚀
